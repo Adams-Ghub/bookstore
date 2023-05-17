c9bceea66c32122ded84903302f6fb732f83f32d
Adams-Ghub <bamfoadamsf@gmail.com>
2023-05-17 12:44:48 +0000

    Develop book addition feature

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
-const BookItem = ({ title, author }) => (
-  <div>
-    <span>{`${title} by `}</span>
-    <span>{author}</span>
-    <input type="button" value="delete" />
-  </div>
-);
+const BookItem = ({ title, author, id }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <span>{`${title} by `}</span>
+      <span>{author}</span>
+      <input type="button" value="delete" onClick={() => dispatch(removeBook(id))} />
+    </div>
+  );
+};
 
 BookItem.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default BookItem;
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,29 +1,13 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import BookList from './BookList';
 import AddBookForm from './AddBookForm';
 
 const Books = () => {
-  const [books, setBooks] = useState([
-    {
-      id: '1',
-      title: 'The Lord of the Rings',
-      author: 'John Snow',
-    },
-    {
-      id: '2',
-      title: 'The Game of Thrones',
-      author: 'John Sterling',
-    },
-    {
-      id: '3',
-      title: 'The Legend of the seeker',
-      author: 'Peter Pan',
-    },
-  ]);
-
+  const books = useSelector((state) => state.books);
   return (
     <div>
-      <BookList allBooks={books} />
+      <BookList allBooks={books.books} />
       <AddBookForm />
     </div>
 
