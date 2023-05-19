8bd0d9ba4e9c8a07f557f27bcdbf874bf4828e72
Adams-Ghub <bamfoadamsf@gmail.com>
2023-05-19 13:30:08 +0000

    Add and Delete from API

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import BookList from './BookList';
 import AddBookForm from './AddBookForm';
+import { getAllBooks } from '../redux/books/booksSlice';
 
 const Books = () => {
-  const books = useSelector((state) => state.books);
+  const { books, isLoading } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const booksArray = Object.keys(books);
+  useEffect(() => {
+    dispatch(getAllBooks());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <BookList allBooks={books.books} />
+      <BookList allBooks={booksArray} />
       <AddBookForm />
     </div>
-
   );
 };
 export default Books;
